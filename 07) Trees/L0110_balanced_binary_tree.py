2c57bb6aa7f41c11dc246c8f065431c71ada61e9
samantharazon <samantharazon2@gmail.com>
2024-10-27 00:23:34 -0400

    L0110

--- 07) Trees/L0110_balanced_binary_tree.py
+++ 07) Trees/L0110_balanced_binary_tree.py
@@ -35,20 +35,43 @@ class TreeNode:
         else:
             return True
 
+
 ###############################################################
+
     def isBalanced(self, root):
-        
+
         def dfs(root):
-            
-            if not root: 
-                return [True, 0]    
-        
-            left = dfs(root.left) 
-            right = dfs(root.right) 
-            
+
+            if not root:
+                return [True, 0]
+
+            left = dfs(root.left)
+            right = dfs(root.right)
             balanced = (left[0] and right[0] and
-                       abs(left[1] - right[1]) <= 1)
+                        abs(left[1] - right[1]) <= 1)
+
+            return [balanced, 1 + max(left[1], right[1])]
+
+        return dfs(root)[0]
+
+###############################################################
+    def isBalanced_LOGS(self, root):
+        def dfs(root):
+            if not root:
+                print("Reached a leaf node, returning [True, 0]")
+                return [True, 0]
+
+            left = dfs(root.left)
+            right = dfs(root.right)
+
+            print(f"Node ({root.value}): BALANCED?\tleft[0]={left[0]}, \tright[0]={right[0]}")
+            print(f"Node ({root.value}): HEIGHT?\tleft[1]= {left[1]}, \tright[1]={right[1]}")
+
+            balanced = (left[0] and right[0] and abs(left[1] - right[1]) <= 1)
             
+            print(f"Node ({root.value}): balanced = {balanced}")
+            print()
+
             return [balanced, 1 + max(left[1], right[1])]
         
         return dfs(root)[0]
